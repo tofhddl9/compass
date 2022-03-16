fc39957c5109d6cf395f48988bb57fc127582d46
Gwang Hun Lee <tofhddl9@gmail.com>
2022-03-16 23:45:28 +0900

    Initial commit

---
+++ app/src/main/java/com/lgtm/default_android_project_template/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lgtm.default_android_project_template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
